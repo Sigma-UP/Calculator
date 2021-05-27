8f5e1c6c414e0335e44e75099e331d179a3e09aa
Sigma-UP <62567341+Sigma-UP@users.noreply.github.com>
2021-05-27 20:36:07 +0300

    added main program

--- Calculator.Test/UnitTest1.cs
+++ Calculator.Test/UnitTest1.cs
@@ -4,35 +4,35 @@ namespace CalculatorMain.Test
 {
     public class Calculations
     {
-        double x = 6, y = 4;
+        const double x = 6, y = 4;
         [Fact]
         public void Division()
         {
-            Assert.Equal(Calculator.Division(x, y), x/y);
-            Assert.Equal(Calculator.Division(y, x), y/x);
-            Assert.Equal(Calculator.Division(x, x), 1);
-            Assert.Equal(Calculator.Division(x, 1), x);
+            Assert.Equal(1.5, Calculator.Division(x, y));
+            Assert.Equal(y / x, Calculator.Division(y, x));
+            Assert.Equal(1, Calculator.Division(x, x));
+            Assert.Equal(6, Calculator.Division(x, 1));
             // Assert.Equal(CalculatorCL.Division(2, 0), ); figure out divide by zero asap
         }
         [Fact]
         public void Multiplication()
         {
-            Assert.Equal(Calculator.Multiplication(x, y), x*y);
-            Assert.Equal(Calculator.Multiplication(x, x), x*x);
-            Assert.Equal(Calculator.Multiplication(x, 1), x);
-            Assert.Equal(Calculator.Multiplication(x, 0.5), x / 2);
-            Assert.Equal(Calculator.Multiplication(x, 0), 0);
+            Assert.Equal(24,    Calculator.Multiplication(x, y));
+            Assert.Equal(36,    Calculator.Multiplication(x, x));
+            Assert.Equal(6,      Calculator.Multiplication(x, 1));
+            Assert.Equal(3,  Calculator.Multiplication(x, 0.5));
+            Assert.Equal(0,      Calculator.Multiplication(x, 0));
         }
         [Fact]
         public void Addition()
         {
-            Assert.Equal(Calculator.Addiction(x, y), x + y);
+            Assert.Equal(10, Calculator.Addiction(x, y));
         }
         [Fact]
         public void Substraction()
         {
-            Assert.Equal(Calculator.Substraction(x, y), x - y);
-            Assert.Equal(Calculator.Substraction(y, x), y - x);
+            Assert.Equal(2, Calculator.Substraction(x, y));
+            Assert.Equal(-2, Calculator.Substraction(y, x));
         }
     }
 }
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 namespace CalculatorMain
 {
+
     public static class Calculator
     {
         public static double Division(double arg_x, double arg_y)
         {
-            return arg_x/arg_y;
+            return arg_x / arg_y;
         }
         public static double Multiplication(double arg_x, double arg_y)
         {
@@ -24,11 +25,45 @@ namespace CalculatorMain
     {
         static void Main(string[] args)
         {
-            int a = 9, b = 2;
-            Console.WriteLine($"D = {Calculator.Division(a, b)}");
-            Console.WriteLine($"M = {Calculator.Multiplication(a, b)}");
-            Console.WriteLine($"A = {Calculator.Addiction(a, b)}");
-            Console.WriteLine($"S = {Calculator.Substraction(a, b)}");
+            double a, b, num;
+            while (true)
+            {
+                Console.Write("Enter a: ");
+                if (Double.TryParse(Console.ReadLine(), out num))
+                {
+                    a = num;
+                    break;
+                }
+            }
+            while (true)
+            {
+                Console.Write("Enter b: ");
+                if (Double.TryParse(Console.ReadLine(), out num))
+                {
+                    b = num;
+                    break;
+                }
+            }
+
+            Console.Write($"Choose operation:\n1. {a} + {b}\n2. {a} - {b}\n3. {a} * {b}\n4. {a} / {b}\nYour choice is ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine($"{a} + {b} = {Calculator.Addiction(a, b)}");
+                    break;
+                case 2:
+                    Console.WriteLine($"{a} + {b} = {Calculator.Substraction(a, b)}");
+                    break;
+                case 3:
+                    Console.WriteLine($"{a} + {b} = {Calculator.Multiplication(a, b)}");
+                    break;
+                case 4:
+                    Console.WriteLine($"{a} / {b} = {Calculator.Division(a, b)}");
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
